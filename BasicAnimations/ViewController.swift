a483d0ac1526e2366e4a98aa02b7ecb941f79224
John Kouris <jdkouris@gmail.com>
2019-09-19 18:35:53 -0700

    Initial Commit

---
+++ BasicAnimations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BasicAnimations
+//
+//  Created by John Kouris on 9/19/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
