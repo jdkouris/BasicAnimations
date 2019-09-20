326a4761fa7eafa27d237d713e59af73bb4667e7
John Kouris <jdkouris@gmail.com>
2019-09-19 19:03:54 -0700

    Refactored the layout constraints using anchors

--- BasicAnimations/ViewController.swift
+++ BasicAnimations/ViewController.swift
@@ -24,12 +24,18 @@ class ViewController: UIViewController {
         // Adding the redView as a subview of the view controller's view
         view.addSubview(redView)
         
-        let leadingConstraint = NSLayoutConstraint(item: redView, attribute: .leading, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .leading, multiplier: 1, constant: 30)
-        let verticalConstraint = NSLayoutConstraint(item: redView, attribute: .top, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .top, multiplier: 1, constant: 0)
-        let heightConstraint = NSLayoutConstraint(item: redView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 100)
-        let aspectRatioConstraint = NSLayoutConstraint(item: redView, attribute: .width, relatedBy: .equal, toItem: redView, attribute: .height, multiplier: 1, constant: 0)
+//        let leadingConstraint = NSLayoutConstraint(item: redView, attribute: .leading, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .leading, multiplier: 1, constant: 30)
+//        let verticalConstraint = NSLayoutConstraint(item: redView, attribute: .top, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .top, multiplier: 1, constant: 0)
+//        let heightConstraint = NSLayoutConstraint(item: redView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 100)
+//        let aspectRatioConstraint = NSLayoutConstraint(item: redView, attribute: .width, relatedBy: .equal, toItem: redView, attribute: .height, multiplier: 1, constant: 0)
+        
+        let leadingConstraint = redView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 30)
+        let verticalConstraint = redView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
+        let heightConstraint = redView.heightAnchor.constraint(equalToConstant: 100)
+        let aspectRatioConstraint = redView.widthAnchor.constraint(equalTo: redView.heightAnchor)
         
         NSLayoutConstraint.activate([leadingConstraint, verticalConstraint, heightConstraint, aspectRatioConstraint])
+        
     }
 
 
